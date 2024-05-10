3334ff250f028bd723d0282fe86c85875a3e2d73
kimdh42 <157452541+kimdh42@users.noreply.github.com>
2024-05-10 15:38:15 +0900

    jpql

---
+++ chap04-jpql/src/main/java/com/ohgiraffers/jpql/section03/projection/EmbeddedMenu.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.jpql.section03.projection;
+
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_Menu")
+public class EmbeddedMenu {
+
+    @Id
+    private int menuCode;
+
+    @Embedded
+    private MenuInfo menuInfo;
+
+    private int categoryCode;
+
+    private String orderableStatus;
+
+    public EmbeddedMenu() {}
+
+}
